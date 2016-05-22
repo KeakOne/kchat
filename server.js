fe436d94790059b2749c674f4e4ebf78ecb547a8
Keakone <keanucarnevale@gmail.com>
2016-05-22 13:23:23 +1200

    moving to node.js

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-var mongo = require('mongodb').MongoClient,
-client = require('socket.io').listen(8080).sockets;
+var express = require('express');
+	app = express()
 
 
-mongo.connect('mongodb://127.0.0.1/chat', function(err,db) {
-	if (err) throw err;
 
-	client.on('connection',function(socket) {
-		//wait for input
-		socket.on('input', function(data){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var mongo = require('mongodb').MongoClient,
+// client = require('socket.io').listen(8080).sockets;
+
+
+// mongo.connect('mongodb://127.0.0.1/chat', function(err,db) {
+// 	if (err) throw err;
+
+// 	client.on('connection',function(socket) {
+// 		//wait for input
+// 		socket.on('input', function(data){
 			
-		})
-	});
-});
+// 		})
+// 	});
+// });
